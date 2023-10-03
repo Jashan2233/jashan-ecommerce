ad597353622fe2b998aa453a705749ec0fa57fcc
Jashan2233 <jashan17100@gmail.com>
2023-10-03 10:51:12 -0500

    first commit + Product model added

---
+++ app/models/product.py
@@ -0,0 +1,22 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.Foreignkey(add_prefix_for_prod('users.id')), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    preview_image = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now)
+
+
+    #Relationships
+    users = db.relationship("User", back_populates="products")
