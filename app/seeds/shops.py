9feab454025db0e01482cf127c7ff88ef45f81b2
Jashan2233 <jashan17100@gmail.com>
2023-10-03 12:04:49 -0500

    seeds for shops added

---
+++ app/seeds/shops.py
@@ -0,0 +1,32 @@
+from app.models import db, Shop, environment, SCHEMA
+from sqlalchemy.sql import text
+
+
+def seed_shops():
+    shop1 = Shop(
+        owner_id = 1,
+        product_id = 1,
+        name = "The TECH LLC"
+    )
+    shop2 = Shop(
+        owner_id = 2,
+        product_id = 2,
+        name = "The TECH LLC"
+    )
+    shop3 = Shop(
+        owner_id = 3,
+        product_id = 3,
+        name = "The TECH LLC"
+    )
+
+    all_shops = [shop1,shop2,shop3]
+    add_shops = [db.session.add(shop) for shop in all_shops]
+    db.session.commit()
+
+def undo_shops():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.shops RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM shops"))
+
+    db.session.commit()
